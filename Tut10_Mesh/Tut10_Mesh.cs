7e630f2c8bd5d1b9b1402ae7f6b95586c6299953
Alistyler <115165426+Alistyler@users.noreply.github.com>
2023-01-09 12:55:55 +0100

    Add files via upload
    Code und Aufschrieb zur Vorlesung und gleichzeitig Übung für das Praktikum.

---
+++ Tut10_Mesh/Tut10_Mesh.cs
@@ -0,0 +1,112 @@
+﻿using Fusee.Base.Common;
+using Fusee.Base.Core;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Engine.Core.Scene;
+using Fusee.Math.Core;
+using Fusee.Serialization;
+using Fusee.Xene;
+using static Fusee.Engine.Core.Input;
+using static Fusee.Engine.Core.Time;
+using Fusee.Engine.Gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuseeApp
+{
+    [FuseeApplication(Name = "Tut10_Mesh", Description = "Yet another FUSEE App.")]
+    public class Tut10_Mesh : RenderCanvas
+    {
+        private SceneContainer _scene;
+        private SceneRendererForward _sceneRenderer;
+        private Transform _baseTransform;
+
+        SceneContainer CreateScene()
+        {
+            // Initialize transform components that need to be changed inside "RenderAFrame"
+
+            _baseTransform = new Transform
+            {
+                Rotation = new float3(0, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 0, 0)
+            };
+
+            // Setup the scene graph
+            return new SceneContainer
+            {
+                Children =
+                {
+                    new SceneNode
+                    {
+                        Name = "Camera",
+                        Components =
+                        {
+                            new Transform
+                            {
+                                Translation = new float3(0, 10, -30),
+                                Rotation = new float3(0.25f, 0, 0)
+                            },
+                            new Camera(ProjectionMethod.Perspective, 5, 100, M.PiOver4)
+                            {
+                                BackgroundColor =  (float4) ColorUint.Greenery
+                            }
+                        }
+                    },
+
+                    new SceneNode
+                    {
+                        Components =
+                        {
+                            // TRANSFORM COMPONENT
+                            _baseTransform,
+
+                            // SHADER EFFECT COMPONENT
+                            MakeEffect.FromDiffuseSpecular((float4) ColorUint.LightGrey),
+
+                            // MESH COMPONENT
+                            new CuboidMesh(new float3(10, 10, 10))
+
+                            /*Triangles: Ein Würfel hat 6 Faces. Diese Faces lassen sich in Triangles (Dreiecke) aufteilen, weil die Grafikkarte so funktioniert. Hier sind 36 Triangles.
+                                         Die Werte von den Triangles sind Verweise auf die Indezes der Vertices. Drei Werte bilden einen Dreieck.
+                                         Lösung: Da Vertices 24 Indezes haben, kommt man auf die 36 Indezes bei den Triangles.*/
+
+                            /*Vertices: Ein typischer Würfel hat 8 Vertices. Aber hier hat es den 3-fachen Wert. Manche Punkte sind doppelt angegeben (s. Debugmode>Watch>(Variable eingeben))
+                                        Lösung: Die 24 Indezes sind da, weil drei Normals eine Vertex besetzen. Da man einen Wert zuordnen kann, erstellt man drei überlappende Vertecies für die drei Normals.
+                                        Wenn man nur eine Normale zu einem Vertex hinzufügt, sieht das so aus wie ein Würfel im Smooth Shader in Blender.*/
+
+                            /*Normals: Einen Normale ist ein Vektor. Diese steht orthogonal senkrecht zur Fläche. Diese sorgt dafür in welche Richtung die Fläche zeigt.
+                                        Die Normals sind Eigenschaften des Cubes und gehören zu den Vertices.*/
+                        }
+                    },
+                }
+            };
+        }
+
+        // Init is called on startup. 
+        public override void Init()
+        {
+            _scene = CreateScene();
+
+            // Create a scene renderer holding the scene above
+            _sceneRenderer = new SceneRendererForward(_scene);
+        }
+
+        // RenderAFrame is called once a frame
+        public override void RenderAFrame()
+        {
+            _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
+
+            // Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            // Render the scene on the current render context
+            _sceneRenderer.Render(RC);
+
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
+            Present();
+        }
+
+    }
+}
